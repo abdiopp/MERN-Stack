cc9f15a9836a8117219c835b92f9c6c3b2495663
Abdullah Murtaza <99031293+abdiopp@users.noreply.github.com>
2023-06-30 23:05:14 +0500

    todo controller and routes setup

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,27 +1,34 @@
+const asyncHandler = require('express-async-handler')
+
 // Get Goals
 // GET/api/todos
 // private
-const getTodos = (req, res) => {
+const getTodos = asyncHandler(async (req, res) => {
+    console.log(req.body);
     res.json({ message: 'Get Todos' })
-}
+})
 // set Goals
 // POST/api/todos
 // private
-const setTodo = (req, res) => {
+const setTodo = asyncHandler(async (req, res) => {
+    if (!req.body.text) {
+        res.status(400)
+        throw new Error('Please add text')
+    }
     res.json({ message: 'set Todos' })
-}
+})
 // Update Goals
 // PUT/api/todos/id
 // private
-const updateTodo = (req, res) => {
+const updateTodo = asyncHandler(async (req, res) => {
     res.json({ message: `Update goal ${req.params.id}` })
-}
+})
 // Delete Goals
 // DELETE/api/todos/id
 // private
-const deleteTodo = (req, res) => {
+const deleteTodo = asyncHandler(async (req, res) => {
     res.json({ message: `Delete goal ${req.params.id}` })
-}
+})
 
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.use('/api/todos', require('./routes/todoRoutes'))
+
+// error handling middleware
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server is running on ${port}`))
