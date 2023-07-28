dbafafd98ff7c5ea8ad313e706642d039d20f2c1
Abdullah Murtaza <99031293+abdiopp@users.noreply.github.com>
2023-07-28 21:36:45 +0500

    complete CRUD and AUTH

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,24 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const { errorHandler } = require('./middleware/errorMiddleware')
-const connectDB = require('./config/db')
-const port = process.env.PORT || 5000
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
+
+// By abdiopp
+
+// connect DB
 
 connectDB();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/todos', require('./routes/todoRoutes'))
-app.use('/api/users', require('./routes/userRoutes'))
+app.use("/api/todos", require("./routes/todoRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 
 // error handling middleware
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server is running on ${port}`))
+app.listen(port, () => console.log(`Server is running on ${port}`));
